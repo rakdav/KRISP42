60ac73c415f6fb6295d7bd6effb3b67e5c470f8b
rakdav <rakdav@inbox.ru>
2026-03-02 17:32:52 +0200

    4

--- screens/clients/components/ButtonAdd.js
+++ screens/clients/components/ButtonAdd.js
@@ -1,2 +1,28 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, TouchableOpacity, Dimensions} from 'react-native';
+const SCREEN_WIDTH = Dimensions.get('window').width;
+export default function ButtonAdd({text,onPress}) {
+    return (
+        <TouchableOpacity style={[styles.button,styles.buttonText]} onPress={onPress}>
+            <Text style={[styles.buttonText,{color:'#FFFFFF'}]}>{text}</Text>
+        </TouchableOpacity>
+    );
+}
+const styles = StyleSheet.create({
+    buttonText: {
+        fontSize: 14,
+        fontWeight: '800',
+        lineHeight:19,
+        alignSelf:'center'
+    },
+    button:{
+        backgroundColor:'#F87360',
+        marginTop:5,
+        padding:15,
+        marginBottom:30,
+        width:SCREEN_WIDTH-30,
+        borderRadius:12,
+        flexDirection:'row',
+        justifyContent:'center'
+    },
+})
