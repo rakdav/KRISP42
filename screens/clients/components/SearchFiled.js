6e861ec50bc4320230a92569dd5cf3bda9d8b27d
rakdav <rakdav@inbox.ru>
2026-03-02 17:39:29 +0200

    5

---
+++ screens/clients/components/SearchFiled.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
+export default function SearchFiled({filter,setFilter}){
+    return (
+        <View style={styles.search}>
+            <Image style={{margin:20,marginRight:10,alignSelf:'center'}}
+            source={require('../../../assets/icon1.png')}/>
+            <TextInput
+                style={styles.textInp}
+                placeholder="Поиск"
+                value={filter}
+                onChangeText={(text) => {
+                    setFilter(text);
+                }}></TextInput>
+        </View>
+    )
+}
+const styles=StyleSheet.create({
+    textInp:{
+        fontSize:16,
+        fontWeight:'400',
+        height:50,
+        fontFamily:'Poppins ',
+        justifyContent:'center',
+    },
+    search:{
+        flexDirection:'row',
+        margin:12,
+        padding:0,
+        backgroundColor:'#F7F7F7'
+    }
+})
