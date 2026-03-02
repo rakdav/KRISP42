d937917c71c1086f8618ca7ce4acd4bb89fbfc6d
rakdav <rakdav@inbox.ru>
2026-03-02 18:36:24 +0200

    9

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileS from './screens/profile';
-import AddingS from './screens/addind';
+import AddingS from './screens/addInd';
 import ClientsS from './screens/clients';
 
 const Stack = createStackNavigator();
